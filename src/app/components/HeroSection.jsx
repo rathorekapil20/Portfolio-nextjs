0a5ac41a140269100f1ea6d7e631eee0b1798db5
Kapil Rathore <116137491+rathorekapil20@users.noreply.github.com>
2024-07-29 11:12:56 +0530

    changes

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -46,7 +46,7 @@ const HeroSection = () => {
           </p>
           <div>
             <Link
-              href="/#contact"
+              href="https://twitter.com/rathorekapilxd" target="_blank"
               className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
             >
               Let&apos;s Connect!
